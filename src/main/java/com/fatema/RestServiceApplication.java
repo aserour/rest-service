21c9717e112365f1ca09ced378e3735db02eb418
Ahmed Sorour <aserour@users.noreply.github.com>
2018-05-02 01:11:50 +0200

    comment servlet support

--- src/main/java/com/fatema/RestServiceApplication.java
+++ src/main/java/com/fatema/RestServiceApplication.java
@@ -1,16 +1,16 @@
 package com.fatema;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.builder.SpringApplicationBuilder;
+// import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class RestServiceApplication  extends SpringBootServletInitializer{
+public class RestServiceApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RestServiceApplication.class);
-	}
+// 	@Override
+// 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+// 		return application.sources(RestServiceApplication.class);
+// 	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
